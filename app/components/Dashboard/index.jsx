6b935cf1358d468a452a7be919aff978868c9e79
Arjun Raju Pillai <arjun@darwinlabs.io>
2018-04-16 00:25:30 +0530

    Create nav buttons for dashboard

--- app/components/Dashboard/index.jsx
+++ app/components/Dashboard/index.jsx
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
+import Button from 'muicss/lib/react/button';
+import classnames from 'classnames';
+
+import styles from './style.scss';
 
 export default class Dashboard extends Component <Props> {
   constructor(props) {
@@ -11,10 +15,15 @@ export default class Dashboard extends Component <Props> {
 
   render() {
     return (
-      <div>
+      <div className={styles['page-container']}>
         <Container>
-          <header>
-            <h2>Karacoin Wallet</h2>
+          <header className={classnames(styles['dashboard-header'], 'd-flex justify-content-between align-items-center')}>
+            <h2 className={styles['dashboard-title']}>Karacoin Wallet</h2>
+            <nav className="ml-auto">
+              <Button className={classnames(styles['nav-button'], styles['send-button'])} variant="raised">Send</Button>
+              <Button className={styles['nav-button']} variant="raised">Receive Kara</Button>
+              <Button className={styles['nav-button']} variant="raised">Logout</Button>
+            </nav>
           </header>
           <Row>
             <Col>
