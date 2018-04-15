739138136dcd1d2fb1245176b59a070af01c76e2
Arjun Raju Pillai <arjun@darwinlabs.io>
2018-04-16 00:58:44 +0530

    Add placeholder panels in the dashboard page

--- app/components/Dashboard/index.jsx
+++ app/components/Dashboard/index.jsx
@@ -2,32 +2,75 @@ import React, { Component } from 'react';
 import { Container, Row, Col } from 'reactstrap';
 import Button from 'muicss/lib/react/button';
 import classnames from 'classnames';
+import PropTypes from 'prop-types';
 
 import styles from './style.scss';
 
-export default class Dashboard extends Component <Props> {
+const DashboardPanel = ({ children, title }) => (
+  <div className={classnames('mui-panel', styles['dashboard-panel'])}>
+    <header className={styles['dashboard-panel--header']}>
+      { title }
+    </header>
+    <main>
+      { children }
+    </main>
+  </div>
+);
+
+DashboardPanel.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]).isRequired,
+  title: PropTypes.string.isRequired,
+};
+
+export default class Dashboard extends Component < Props > {
   constructor(props) {
     super(props);
-    this.state = {
-
-    };
+    this.state = {};
   }
 
   render() {
     return (
       <div className={styles['page-container']}>
         <Container>
-          <header className={classnames(styles['dashboard-header'], 'd-flex justify-content-between align-items-center')}>
+          <header
+            className={classnames(styles['dashboard-header'], 'd-flex justify-content-between align-items-center')}>
             <h2 className={styles['dashboard-title']}>Karacoin Wallet</h2>
             <nav className="ml-auto">
-              <Button className={classnames(styles['nav-button'], styles['send-button'])} variant="raised">Send</Button>
+              <Button
+                className={classnames(styles['nav-button'], styles['send-button'])}
+                variant="raised"
+              >
+                Send
+              </Button>
               <Button className={styles['nav-button']} variant="raised">Receive Kara</Button>
               <Button className={styles['nav-button']} variant="raised">Logout</Button>
             </nav>
           </header>
           <Row>
             <Col>
-              Page Loaded!
+              <DashboardPanel title="Address">
+                <div className={styles['grey-data-panel']}>
+                  12214044115343504305L
+                </div>
+              </DashboardPanel>
+            </Col>
+            <Col>
+              <DashboardPanel title="Peer">
+                <div className={styles['grey-data-panel']}>
+                  <header>Mainnet</header>
+                  node05.karachain.io : 443
+                </div>
+              </DashboardPanel>
+            </Col>
+            <Col>
+              <DashboardPanel title="Balance">
+                <div className={styles['grey-data-panel']}>
+                  0 Kara
+                </div>
+              </DashboardPanel>
             </Col>
           </Row>
         </Container>
